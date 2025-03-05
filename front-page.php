6f1b873f932476eec13c629327397ead8b5fa653
Olehas <odyakun@gmail.com>
2025-03-05 23:57:37 +0200

    initial

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<main class="wrapper home_page"><span class="opacity-bg"></span>
+
+    <section class="container ">
+
+<!--        New Block-->
+
+    </section>
+
+</main>
+<?php get_footer(); ?>
